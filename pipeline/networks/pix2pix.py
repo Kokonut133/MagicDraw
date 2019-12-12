fc83b00d31728b65f57b963d911d6e3d6077a9d5
Christian Stur <stur@exb.de>
2019-12-12 16:55:06 +0100

    discriminator should work; unsure of generator

--- pipeline/networks/pix2pix.py
+++ pipeline/networks/pix2pix.py
@@ -1,9 +1,12 @@
 import datetime
 import os
+from glob import glob
+
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy
 from keras.layers import BatchNormalization
-from keras.layers import Input, Dropout, Concatenate
+from keras.layers import Input, Concatenate
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import UpSampling2D, Conv2D
 from keras.models import Model
@@ -11,29 +14,26 @@ from keras.models import Model
 
 class Pix2Pix:
     def __init__(self, image_shape):
-        self.img_shape = image_shape
-        if len(self.img_shape) != 3:
-            print("Image should be w, h, c but is " + str(self.img_shape))
+        self.imgenerator_shape = image_shape
+        if len(self.imgenerator_shape) != 3:
+            print("Image should be w, h, c but is " + str(self.imgenerator_shape))
 
-        self.discriminator = self.build_discriminator(input_shape=self.img_shape)
+        patchsize = int(self.imgenerator_shape[0]/16)   # 16 parts of the image are evaluated if real
+        self.disc_patch = (patchsize, patchsize, 1)
+
+        self.discriminator = self.buildiscriminator_discriminator(input_shape=self.imgenerator_shape)
         self.discriminator.compile(loss="mse", optimizer="adam", metrics=["accuracy"])
-        self.generator = self.build_generator(input_shape=self.img_shape)
+        self.generator = self.buildiscriminator_generator(input_shape=self.imgenerator_shape)
 
-        img_A = Input(shape=self.img_shape)
-        img_B = Input(shape=self.img_shape)
-
+        img_A = Input(shape=self.imgenerator_shape)
+        img_B = Input(shape=self.imgenerator_shape)
         fake_A = self.generator(img_B)
 
-        # For the combined model we will only train the generator
-        self.discriminator.trainable = False
-
-        # Discriminators determines validity of translated images / condition pairs
         valid = self.discriminator([fake_A, img_B])
-
         self.combined = Model(inputs=[img_A, img_B], outputs=[valid, fake_A])
         self.combined.compile(loss=["mse", "mae"], loss_weights=[1, 100], optimizer="adam")
 
-    def build_generator(self, input_shape):
+    def buildiscriminator_generator(self, input_shape):
         def conv2d(input, filters, batch_norm, k_size=4):
             d = Conv2D(filters, kernel_size=k_size, strides=2, padding="same")(input)
             d = LeakyReLU(alpha=0.2)(d)
@@ -73,7 +73,7 @@ class Pix2Pix:
         return Model(input, output_img)
 
     def build_discriminator(self, input_shape):
-        def d_layer(input, filters, batch_norm, f_size=4):
+        def discriminator_layer(input, filters, batch_norm, f_size=4):
             d = Conv2D(filters, kernel_size=f_size, strides=2, padding="same")(input)
             d = LeakyReLU(alpha=0.2)(d)
             if batch_norm:
@@ -86,60 +86,40 @@ class Pix2Pix:
 
         n = input_shape[0]
 
-        d1 = d_layer(input=combined_imgs, filters=n, batch_norm=False)
-        d2 = d_layer(input=d1, filters=n * 2, batch_norm=True)
-        d3 = d_layer(input=d2, filters=n * 4, batch_norm=True)
-        d4 = d_layer(input=d3, filters=n * 8, batch_norm=True)
+        d1 = discriminator_layer(input=combined_imgs, filters=n, batch_norm=False)
+        d2 = discriminator_layer(input=d1, filters=n * 2, batch_norm=True)
+        d3 = discriminator_layer(input=d2, filters=n * 4, batch_norm=True)
+        d4 = discriminator_layer(input=d3, filters=n * 8, batch_norm=True)
 
         validity = Conv2D(1, kernel_size=4, strides=1, padding="same")(d4)
 
         return Model([img_A, img_B], validity)
 
-    def train(self, epochs, batch_size=1, sample_interval=50):
-
+    def train(self, epochs, data_dir, batch_size=1, sample_interval=50):
         start_time = datetime.datetime.now()
 
-        # Adversarial loss ground truths
         valid = np.ones((batch_size,) + self.disc_patch)
         fake = np.zeros((batch_size,) + self.disc_patch)
 
         for epoch in range(epochs):
-            for batch_i, (imgs_A, imgs_B) in enumerate(self.data_loader.load_batch(batch_size)):
-
-                # ---------------------
-                #  Train Discriminator
-                # ---------------------
-
-                # Condition on B and generate a translated version
+            for count, (imgs_A, imgs_B) in enumerate(self.load_batch(data_dir=data_dir, batch_size=batch_size)):
                 fake_A = self.generator.predict(imgs_B)
 
-                # Train the discriminators (original images = real / generated = Fake)
-                d_loss_real = self.discriminator.train_on_batch([imgs_A, imgs_B], valid)
-                d_loss_fake = self.discriminator.train_on_batch([fake_A, imgs_B], fake)
-                d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+                discriminator_loss_real = self.discriminator.train_on_batch([imgs_A, imgs_B], valid)
+                discriminator_loss_fake = self.discriminator.train_on_batch([fake_A, imgs_B], fake)
+                discriminator_loss = 0.5 * np.add(discriminator_loss_real, discriminator_loss_fake)
 
-                # -----------------
-                #  Train Generator
-                # -----------------
-
-                # Train the generators
-                g_loss = self.combined.train_on_batch([imgs_A, imgs_B], [valid, imgs_A])
+                generator_loss = self.combined.train_on_batch([imgs_A, imgs_B], [valid, imgs_A])
 
                 elapsed_time = datetime.datetime.now() - start_time
-                # Plot the progress
-                print("[Epoch %d/%d] [Batch %d/%d] [D loss: %f, acc: %3d%%] [G loss: %f] time: %s" % (epoch, epochs,
-                                                                                                      batch_i,
-                                                                                                      self.data_loader.n_batches,
-                                                                                                      d_loss[0],
-                                                                                                      100 * d_loss[1],
-                                                                                                      g_loss[0],
-                                                                                                      elapsed_time))
+                print("[Epoch %d/%d] [D loss real: %f; fake: %f] [G loss: %f] time: %s" %
+                      (epoch, epochs, discriminator_loss[0], discriminator_loss[1], generator_loss[0], elapsed_time))
 
                 # If at save interval => save generated image samples
-                if batch_i % sample_interval == 0:
-                    self.sample_images(epoch, batch_i)
+                if count % sample_interval == 0:
+                    self.show_results(epoch, count)
 
-    def sample_images(self, epoch, batch_i):
+    def show_results(self, epoch, batch_i):
         os.makedirs("images/%s" % self.dataset_name, exist_ok=True)
         r, c = 3, 3
 
@@ -160,5 +140,64 @@ class Pix2Pix:
                 axs[i, j].set_title(titles[i])
                 axs[i, j].axis("off")
                 cnt += 1
-        fig.savefig("images/%s/%d_%d.png" % (self.dataset_name, epoch, batch_i))
+        fig.savefig("images/%s/%discriminator_%d.png" % (self.dataset_name, epoch, batch_i))
         plt.close()
+
+    def load_data(self, batch_size=1, is_testing=False):
+        path = glob(self.data_dir+"*")
+        batch_images = np.random.choice(path, size=batch_size)
+
+        imgs_A = []
+        imgs_B = []
+        for img_path in batch_images:
+            img = self.load_img_as_np(img_path)
+
+            h, w, _ = img.shape
+            _w = int(w/2)
+            img_A, img_B = img[:, :_w, :], img[:, _w:, :]
+
+            img_A = scipy.misc.imresize(img_A, self.img_res)
+            img_B = scipy.misc.imresize(img_B, self.img_res)
+
+            if not is_testing and np.random.random() < 0.5:
+                img_A = np.fliplr(img_A)
+                img_B = np.fliplr(img_B)
+
+            imgs_A.append(img_A)
+            imgs_B.append(img_B)
+
+        imgs_A = np.array(imgs_A)/127.5 - 1.
+        imgs_B = np.array(imgs_B)/127.5 - 1.
+
+        return imgs_A, imgs_B
+
+    def load_batch(self, data_dir, batch_size=1, is_testing=False):
+        paths = glob(data_dir+"*")
+
+        for i in range(len(paths)-1):
+            batch = paths[i*batch_size:(i+1)*batch_size]
+            imgs_A, imgs_B = [], []
+            for img in batch:
+                img = self.load_img_as_np(img)
+                h, w, _ = img.shape
+                half_w = int(w/2)
+                img_A = img[:, :half_w, :]
+                img_B = img[:, half_w:, :]
+
+                img_A = scipy.misc.imresize(img_A, self.img_res)
+                img_B = scipy.misc.imresize(img_B, self.img_res)
+
+                if not is_testing and np.random.random() > 0.5:
+                        img_A = np.fliplr(img_A)
+                        img_B = np.fliplr(img_B)
+
+                imgs_A.append(img_A)
+                imgs_B.append(img_B)
+
+            imgs_A = np.array(imgs_A)/127.5 - 1.
+            imgs_B = np.array(imgs_B)/127.5 - 1.
+
+            yield imgs_A, imgs_B
+
+    def load_img_as_np(self, path):
+        return scipy.misc.load_img_as_np(path, mode='RGB').astype(np.float)
