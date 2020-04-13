e99c309ab79b340a71c4760ead2c5ed1fe95e431
Kokonut133 <christianstur@gmail.com>
2020-04-13 19:19:52 +0200

    setting up automatic data cleanup over mturks API

---
+++ pipeline/aws_services/aws_image_processor.py
@@ -0,0 +1,73 @@
+# To clean imagesets from false images
+import datetime
+
+import boto3
+import third_party.aws_keys as credentials
+
+__author__ = "cstur"
+
+
+class AWS_Imageprocessor:
+    def __init__(self, testing=True):
+        self.testing = testing
+        session = boto3.Session()
+
+        # us east 1 seems the only place for mturk
+        if self.testing:
+            client = session.client('mturk',
+                region_name="us-east-1",
+                aws_access_key_id=credentials.AWSAccessKeyId,
+                aws_secret_access_key=credentials.AWSSecretKey,
+                endpoint_url="https://mturk-requester-sandbox.us-east-1.amazonaws.com/",
+            )
+            manage_url = "https://requestersandbox.mturk.com/mturk/manageHITs"
+
+        else:
+            client = session.client('mturk',
+                region_name="us-east-1",
+                aws_access_key_id=credentials.AWSAccessKeyId,
+                aws_secret_access_key=credentials.AWSSecretKey,
+                endpoint_url="https://mturk-requester.us-east-1.amazonaws.com",
+            )
+            manage_url = "https://requester.mturk.com/mturk/manageHITs"
+
+        print("Account balance is \n", client.get_account_balance())
+
+    def create_task(self, instructions:str, min_approval_rating:int=90):
+        # @param instructions
+
+        worker_requirements = [{
+            'QualificationTypeId': '000000000000000000L0',
+            'Comparator': 'GreaterThanOrEqualTo',
+            'IntegerValues': [min_approval_rating],
+            'RequiredToPreview': True,
+        }]
+        #
+        # # Create the HIT
+        # response = client.create_hit(
+        #     MaxAssignments=3,
+        #     LifetimeInSeconds=600,
+        #     AssignmentDurationInSeconds=600,
+        #     Reward=mturk_environment['reward'],
+        #     Title='Answer a simple question',
+        #     Keywords='question, answer, research',
+        #     Description='Answer a simple question. Created from mturk-code-samples.',
+        #     Question=question_sample,
+        #     QualificationRequirements=worker_requirements,
+        # )
+        #
+        # # The response included several fields that will be helpful later
+        # hit_type_id = response['HIT']['HITTypeId']
+        # hit_id = response['HIT']['HITId']
+        # print
+        # "\nCreated HIT: {}".format(hit_id)
+        #
+        # print
+        # "\nYou can work the HIT here:"
+        # print
+        # mturk_environment['preview'] + "?groupId={}".format(hit_type_id)
+        #
+        # print
+        # "\nAnd see results here:"
+        # print
+        # mturk_environment['manage']
--- pipeline/data_gatherers/imageset_creator.py
+++ pipeline/data_gatherers/imageset_creator.py
@@ -2,7 +2,7 @@
 # from icrawler.builtin import GoogleImageCrawler                 # currently broken 4.2.2020
 # from pipeline.data_gatherers.image_downloader import ImageDownloader    # urllib.error.URLError: <urlopen error [WinError 10061] No connection could be made because the target machine actively refused it>; cant fix it
 from pipeline.data_gatherers.bing_scraper import ImageDownloader
-from pipeline.aws_services.aws_cleaner import AWS_Cleaner
+from pipeline.aws_services.aws_image_processor import AWS_Imageprocessor
 import os
 
 import settings
@@ -16,18 +16,18 @@ class ImagesetCreator():
 
     def create_image_dataset(self, keyword, image_dir, size, amount, prefix=None, height=None, width=None):
         image_downloader = ImageDownloader()
-        if height and width:
-            exact_size = str(height)+","+str(width)
-            image_downloader.download_from_bing(keyword=keyword, image=image_dir, amount=amount, exact_size=exact_size,
-                                                prefix=prefix,
-                                                chromedriver=os.path.join(settings.third_party_dir, "chromedriver.exe"))
-        else:
-            image_downloader.download_from_bing(keyword=keyword, image_dir=image_dir, amount=amount, size=size,
-                                                prefix=prefix,
-                                                chromedriver=os.path.join(settings.third_party_dir, "chromedriver.exe"))
+        # if height and width:
+        #     exact_size = str(height)+","+str(width)
+        #     image_downloader.download_from_bing(keyword=keyword, image=image_dir, amount=amount, exact_size=exact_size,
+        #                                         prefix=prefix,
+        #                                         chromedriver=os.path.join(settings.third_party_dir, "chromedriver.exe"))
+        # else:
+        #     image_downloader.download_from_bing(keyword=keyword, image_dir=image_dir, amount=amount, size=size,
+        #                                         prefix=prefix,
+        #                                         chromedriver=os.path.join(settings.third_party_dir, "chromedriver.exe"))
 
         # AWS Clean Image-set
-        image_cleaner = AWS_Cleaner(testing=True)
+        image_cleaner = AWS_Imageprocessor(testing=True)
 
 
 
